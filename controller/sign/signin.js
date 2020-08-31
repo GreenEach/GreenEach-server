3f54eb913383abafd5ca85132d81e28db1f51884
Jin Soo Park <jinsoopark@Jinui-MacBookPro.local>
2020-08-31 23:01:03 +0900

    signin.js파일 추가

---
+++ controller/sign/signin.js
@@ -0,0 +1,36 @@
+const db = require('../../models');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+post :(request, response) => {
+  let hashedPassword = request.body.password;
+  var shasum = crypto.createHmac('sha512', 'thisismysecretkey');
+  shasum.update(hashedPassword);
+  hashedPassword = shasum.digest('hex');
+
+  db.User.findOne({ where: { email: request.body.email } })
+  .then((user) => {
+    if (user) {
+      if (user.password === hashedPassword) {
+        let token = jwt.sign(
+          {
+            email: request.body.email,
+          },
+          process.env.TOKEN_KEY
+        );
+
+        response.status(200);
+        response.cookie('userInfo', token);
+        response.json({ message: 'Login Success' });
+      } else {
+        response
+          .status(401)
+          .json({ message: 'Unauthorized error, incorrect password' });
+      }
+    } else {
+      response.status(401).json({ message: 'Unauthorized error, incorrect email' });
+    }
+  });
+}
+}
