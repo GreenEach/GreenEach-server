a487a7628a912a97943df4cf6f92ec92c57b3112
Jin Soo Park <jinsoopark@Jinui-MacBookPro.local>
2020-08-31 22:54:33 +0900

    :heavy_plus_sign: 로그인 컨트롤러 구현
    resolve #6

--- routes/sign.js
+++ routes/sign.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const {signController} = require('../controller');
 
 router.post('/signup', signController.signup.post);
+router.post('/signin', signController.signin.post);
 
 module.exports = router;
