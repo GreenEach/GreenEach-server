96e881cf9497adb929152d0161980032cb80830c
Jin Soo Park <jinsoopark@Jinui-MacBookPro.local>
2020-09-03 12:16:39 +0900

    :heavy_plus_sign::recycle: 게시글 상세 정보 불러오기 기능 구현, 댓글 작성 api 부분 수정  - 게시글 상세 정보 불러오기 기능 구현  - 댓글 작성 api에서 요청에 적절한 contentId가 포함되었는지 확인하도록 수정
    resolve #16

---
+++ controller/content/getContentDetail.js
@@ -0,0 +1,36 @@
+const db = require('../../models');
+
+module.exports = {
+  post: (request, response) => {
+
+    let requestBody = request.body;
+    let reqeustBodyKeys = Object.keys(requestBody);
+    let neccessaryKeys = ["contentId"]
+    
+    neccessaryKeys.forEach(element => {
+      if(reqeustBodyKeys.includes(element) === false){
+        return response.status(400).json({message:"bad request"})
+      }
+    })
+    
+    for(let key in requestBody){
+      if(requestBody[key] === '' || typeof requestBody[key] !== "number"){
+        return response.status(400).json({message:"bad request"})
+      }
+    }
+
+    db.Content.findAll({
+      where:{id: request.body.contentId},
+      attributes:["id", "title", "content", "level", "season", "category", "photoUrl", "createdAt", "updatedAt"],
+      include:[
+        {model: db.User, attributes: ["username"]},
+        {model: db.Comment,
+         attributes: ["id", "comment", "photoUrl", "createdAt"],
+         include: [{model: db.User, attributes: ["username"]}] 
+        },
+      ]
+    }).then(contentDetail => {
+      response.status(200).json(contentDetail);
+    })
+  }
+}
--- controller/content/index.js
+++ controller/content/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-  postContent: require('./postcontent'),
+  postContent: require('./postContent'),
+  getContentDetail: require('./getContentDetail')
 }
