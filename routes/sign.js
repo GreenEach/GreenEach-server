594755076755e0d67d5e2f3366ebbce0d5cced46
jinchuu1391 <62422486+jinchuu1391@users.noreply.github.com>
2020-09-18 13:41:18 +0900

    :heavy_plus_sign: 구글 로그인 기능 추가

--- routes/sign.js
+++ routes/sign.js
@@ -5,6 +5,7 @@ const { signController } = require('../controller');
 
 router.post('/signup', signController.signup.post);
 router.post('/signin', signController.signin.post);
+router.post('/oauth', signController.oauth.post);
 router.use('/', authMiddleware);
 router.post('/signout', signController.signout.post);
 router.post('/mypage', signController.mypage.post);
